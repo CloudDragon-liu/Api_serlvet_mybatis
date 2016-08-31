206473e9112e793720bb6d8fe73612228709c05a
liuyunlong <liuyunlong1126@163.com>
2016-08-31 21:12:47 +0800

    首次上传

---
+++ src/com/yunlong/liu/servlet/UserServlet.java
@@ -0,0 +1,30 @@
+package com.yunlong.liu.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class UserServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doPost(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setCharacterEncoding("UTF-8");
+		resp.setContentType("application/json; charset=utf-8");
+		PrintWriter writer = resp.getWriter();
+		String dataJson = "{\"name\":\"fly\",\"type\":\"虫子\"}";
+		writer.write(dataJson);
+		writer.close();
+	}
+
+}
